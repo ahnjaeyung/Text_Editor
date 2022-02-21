bf896bbec2fb48e37921bbc8def9e8d477863e3e
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-02-21 13:50:00 -0600

    minor inconsequential changes

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,7 +5,6 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
     window.deferredPrompt = event;
-
     // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
 });
